73a83562dae6df6800c08a97dac7fe0cf5ae2358
Isabeli Rezende <isabelirezende30@gmail.com>
2025-12-04 22:48:11 -0300

    Update product images to local file paths

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const clientes = [
 
 // PRODUTOS DO SEU BANCO COM IMAGENS
 const produtos = [
-    { id: 1, nome: "Arroz 5kg", preco: 22.90, img: "https://http2.mlstatic.com/D_NQ_NP_936615-MLA99918197829_112025-O.webp"},
-    { id: 2, nome: "Feijão 1kg", preco: 8.50, img: "https://www.arenaatacado.com.br/on/demandware.static/-/Sites-storefront-catalog-sv/default/dwd863e4e7/Produtos/15031-7896006751113-feijao%20preto%20camil%20pacote%201%20kg-camil-1.jpg" },
-    { id: 3, nome: "Açúcar 2kg", preco: 6.20, img: "https://paulistaoatacadista.vtexassets.com/arquivos/ids/311586/ACUCARCRISTALCRISTALCUCAR2KG1.jpg?v=638082791854230000" },
-    { id: 4, nome: "Leite 1L", preco: 4.99, img: "https://www.davo.com.br/ccstore/v1/images/?source=/file/v1458764921298408807/products/prod_7898215157403.imagem1.jpg&height=940&width=940" },
-    { id: 5, nome: "Café 500g", preco: 12.00, img: "https://m.media-amazon.com/images/I/61ybzna4AML._AC_UF894,1000_QL80_.jpg" },
+    { id: 1, nome: "Arroz 5kg", preco: 22.90, img: "arroz.jpg"},
+    { id: 2, nome: "Feijão 1kg", preco: 8.50, img: "feijao.jpg" },
+    { id: 3, nome: "Açúcar 2kg", preco: 6.20, img: "acucar.jpg" },
+    { id: 4, nome: "Leite 1L", preco: 4.99, img: "leite.jpg" },
+    { id: 5, nome: "Café 500g", preco: 12.00, img: "cafe.jpg" },
 ];
 
 let total = 0;
